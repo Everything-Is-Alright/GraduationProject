ada9f8f86b5a494fad01bb8d11f6929cad051a29
Nyarlathotep <1256128087@qq.com>
2025-12-18 21:06:39 +0800

    写了一个主属性脚本
    现在还有问题，明天看

--- Assets/Script/EntityStats.cs
+++ Assets/Script/EntityStats.cs
@@ -3,16 +3,12 @@ using UnityEngine;
 public class EntityStats : MonoBehaviour
 {
     public Stat maxHp;
-    public Stat vitality;
+    public StatMajor major;
 
     public float GetMaxHealth()
     {
         float baseHp = maxHp.GetValue();
-        if(baseHp == null)
-        {
-            Debug.Log("baseHpÎª¿Õ£¡");
-        }
-        float bonusHp = vitality.GetValue() * 5;
+        float bonusHp = major.vitality.GetValue() * 5;
         //Debug.Log(baseHp + " " + bonusHp);
         return baseHp + bonusHp;
     }
---
+++ Assets/Script/StatsSystem/StatMajor.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class StatMajor
+{    
+    public Stat strength;
+    public Stat agility;
+    public Stat intelligence;
+    public Stat vitality;
+}
