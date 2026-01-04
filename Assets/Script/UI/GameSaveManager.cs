950cab83947e324556626989f905dcb111b9b38f
Nyarlathotep <1256128087@qq.com>
2026-01-04 18:15:03 +0800

    继续更新背包逻辑
    现在把框架搭好了，接下来写具体实现

--- Assets/Script/UI/GameSaveManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
-using System.IO;
-
-public class GameSaveManager : MonoBehaviour
-{
-    public Inventory myInventory;
-}
---
+++ Assets/Script/UI/UIConst.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class UIConst
+{
+    public const string PackagePanel = "PackagePanel";
+}
